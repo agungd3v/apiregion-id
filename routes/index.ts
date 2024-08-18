3809f4582e11cd28054d7feb7c5ef685e76e8d56
agungd3v <agungd3v@gmail.com>
2024-08-18 18:18:33 +0700

    fix route request

--- routes/index.ts
+++ routes/index.ts
@@ -1,9 +1,9 @@
-import { Router, Response } from "express";
+import { Router, Request, Response } from "express";
 import controller from "../controller";
 
 const router = Router();
 
-router.get("/api", (req, res: Response) => res.status(200).json({message: "api wilayan indonesia"}));
+router.get("/api", (req: Request, res: Response) => res.status(200).json({message: "api wilayan indonesia"}));
 router.get("/api/province", controller.provinces);
 router.get("/api/regency", controller.regencies);
 router.get("/api/district", controller.districts);
